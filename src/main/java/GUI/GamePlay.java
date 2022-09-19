4c91b186249734786353264fa0b9679878a8421a
Kyle Ruane <108101096+ruanek@users.noreply.github.com>
2022-09-19 13:58:01 -0700

    Refactored GUI to create a persistent JFrame and options menu, and changed TitleScreen name to StartMenu. , AB#968.

---
+++ src/main/java/GUI/GamePlay.java
@@ -0,0 +1,19 @@
+package main.java.GUI;
+
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
+
+public class GamePlay {
+
+
+
+
+    public GamePlay(JFrame frame) {
+
+
+
+    }
+}
--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -1,32 +1,38 @@
 package main.java.client;
 
 
+import main.java.GUI.GUI;
 import main.java.controller.GameSettings;
 import main.java.view.Music;
 import main.java.view.Story;
 
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
+import java.awt.*;
 import java.io.IOException;
 
 public class Client {
 
     /* This is the main class of the game. It is responsible for starting the game. */
-    public static void main(String[] args) throws IOException, UnsupportedAudioFileException, LineUnavailableException {
-        GameSettings gameSettings = new GameSettings();
-        Music music = new Music();
-        music.playMusic();
-        gameSettings.readGameStory();
-        gameSettings.startGame();
-        gameSettings.endGame();
-
-    }
+//    public static void main(String[] args) throws IOException, UnsupportedAudioFileException, LineUnavailableException {
+//        GameSettings gameSettings = new GameSettings();
+//        Music music = new Music();
+//        music.playMusic();
+//        gameSettings.readGameStory();
+//        gameSettings.startGame();
+//        gameSettings.endGame();
+//
+//    }
     public static void repeatGame() throws IOException {
         GameSettings gameSettings = new GameSettings();
         Story story = new Story();
         story.selectDifficulty();
         gameSettings.startGame();
         gameSettings.endGame();
+    }
+
+    public static void main(String[] args) throws UnsupportedAudioFileException, LineUnavailableException, IOException, FontFormatException {
+        new GUI();
     }
 }
 
