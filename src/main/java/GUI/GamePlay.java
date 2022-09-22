9d883cf2b97a953bf08d2fdfad308556c22bd5df
Kyle Ruane <108101096+ruanek@users.noreply.github.com>
2022-09-22 10:13:55 -0700

    Fixed updated methods to update image and location details when player moves. Added checkAttack method to allow werewolfs to attack upon moving into a room, moved map creation to its own class, AB#1142.

--- src/main/java/GUI/GamePlay.java
+++ src/main/java/GUI/GamePlay.java
@@ -2,20 +2,15 @@ package main.java.GUI;
 
 import main.java.model.Room;
 import main.java.model.RoomMovement;
-import main.java.view.TextColor;
 
 import javax.swing.*;
 
 import java.awt.*;
 import java.io.IOException;
 import java.net.URL;
-import java.security.cert.PolicyNode;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.ResourceBundle;
-
-import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
-import static main.java.GUI.PanelSetup.imagePanel;
 
 public class GamePlay {
 
@@ -24,7 +19,7 @@ public class GamePlay {
     URL hallwayImage = ClassLoader.getSystemClassLoader().getResource("Images/Hallway.jpeg");
 
     JPanel leftPanel;
-    JPanel imagePanel;
+    static JPanel imagePanel;
     static JPanel gameDescriptionPanel;
 
     JPanel rightPanel;
@@ -44,13 +39,14 @@ public class GamePlay {
         Controller gameController = new Controller();
         Room room = RoomMovement.roomSwitcher;
 
-        imagePanel = PanelSetup.imagePanel();
+        imagePanel = PanelSetup.imagePanel(room, gameController);
         gameDescriptionPanel = PanelSetup.gameDescriptionPanel(room);
         healthAndTimePanel = PanelSetup.healthAndTimePanel(Controller.player.getHealth(), Controller.timer);
         statPanel = PanelSetup.statPanel(Controller.player.getAttackPower(), Controller.player.getArmorRating());
         locationPanel = PanelSetup.locationPanel(room);
         compassPanel = PanelSetup.compassPanel(gameController, room);
         inventoryPanel = PanelSetup.inventoryPanel();
+        mapButtonPanel = Map.getMap();
 
         leftPanel = new JPanel();
         leftPanel.setBounds(0, 50, 700, 950);
@@ -65,72 +61,6 @@ public class GamePlay {
         rightPanel.setBackground(Color.blue);
         rightPanel.setLayout(null);
 
-        /*************************************/
-        mapButtonPanel = new JPanel();
-        mapButtonPanel.setBounds(700, 945, 300, 75);
-        mapButtonPanel.setBackground(Color.gray);
-        mapButtonPanel.setOpaque(true);
-
-        JButton mapButton = new JButton("MAP");
-        mapButtonPanel.add(mapButton);
-
-        mapButton.addActionListener(e -> {
-            JFrame mapFrame = new JFrame("Map");
-            mapFrame.setSize(600, 300);
-            mapFrame.setVisible(true);
-            mapFrame.setResizable(false);
-            mapFrame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-
-            String mapRoom = RoomMovement.currentRoom;
-            HashMap<String, Room> allRooms = RoomMovement.getAllRooms();
-            ArrayList<String> roomList = new ArrayList<>(allRooms.keySet());
-            int index = roomList.indexOf(mapRoom);
-            int size = roomList.get(index).length();
-            StringBuilder yourLocation = new StringBuilder();
-            for (int i = 0; i < size-9; i++){
-                yourLocation.append(" ");
-            }
-            roomList.set(index, "* YOU *" + yourLocation);
-
-            JTextArea mapText = new JTextArea();
-            mapText.setLineWrap(true);
-            mapText.setFont(new Font(Font.DIALOG, Font.BOLD, 13));
-            mapText.setForeground(Color.red);
-            mapText.setWrapStyleWord(true);
-            mapText.setText("\t\t" + roomList.get(13));
-            mapText.append("\n\t\t===============");
-            mapText.append("\n" + roomList.get(9));
-            mapText.append("\t\t"+ roomList.get(10));
-            mapText.append("\t\t" + roomList.get(4));
-            mapText.append("\n===============");
-            mapText.append("\t============");
-            mapText.append("\t=============");
-            mapText.append("\n" + roomList.get(14));
-            mapText.append("\t\t"+ roomList.get(3));
-            mapText.append("\t\t" + roomList.get(12));
-            mapText.append("\n===============");
-            mapText.append("\t=============");
-            mapText.append("\t=============");
-            mapText.append("\n" + roomList.get(0));
-            mapText.append("\t\t"+ roomList.get(11));
-            mapText.append("\t\t" + roomList.get(8));
-            mapText.append("\n===============");
-            mapText.append("\t=============");
-            mapText.append("\t=============");
-            mapText.append("\n" + roomList.get(7));
-            mapText.append("\t\t"+ roomList.get(1));
-            mapText.append("\t\t" + roomList.get(5));
-            mapText.append("\n===============");
-            mapText.append("\t=============");
-            mapText.append("\t=============");
-            mapText.append("\n" + roomList.get(2));
-            mapText.append("\t\t"+ roomList.get(6));
-            mapText.append("\t\t" + roomList.get(15));
-            mapFrame.add(mapText, BorderLayout.CENTER);
-
-
-        });
-        /********************************/
 
         rightPanel.add(healthAndTimePanel);
         rightPanel.add(statPanel);
